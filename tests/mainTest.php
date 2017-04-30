2e3c0c376635c6d7e3f282bf3a731bd7a89c04b0
Tomoya Koyanagi <tomk79@gmail.com>
2017-04-30 15:46:22 +0900

    (開発のための前準備)

---
+++ tests/mainTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * test for tomk79\px2-publish-for-multi-device
+ */
+class mainTest extends PHPUnit_Framework_TestCase{
+	private $fs;
+
+	public function setup(){
+		mb_internal_encoding('UTF-8');
+		$this->fs = new tomk79\filesystem();
+	}
+
+
+
+
+	/**
+	 * test
+	 */
+	public function testMain(){
+
+		// -------------------
+		// api.get.vertion
+		$output = $this->passthru( [
+			'php',
+			__DIR__.'/testdata/standard/.px_execute.php' ,
+			'/' ,
+		] );
+		clearstatcache();
+
+		// var_dump($output);
+		$this->assertTrue( $this->common_error( $output ) );
+
+		$this->assertTrue( true );
+
+
+		// 後始末
+		$output = $this->passthru( [
+			'php', __DIR__.'/testdata/standard/.px_execute.php', '/?PX=clearcache'
+		] );
+		clearstatcache();
+		$this->assertTrue( $this->common_error( $output ) );
+		$this->assertTrue( !is_dir( __DIR__.'/testdata/standard/caches/p/' ) );
+		$this->assertTrue( !is_dir( __DIR__.'/testdata/standard/px-files/_sys/ram/caches/sitemaps/' ) );
+	}//testMain();
+
+
+
+
+
+	/**
+	 * PHPがエラー吐いてないか確認しておく。
+	 */
+	private function common_error( $output ){
+		if( preg_match('/'.preg_quote('Parse error:', '/').'/si', $output) ){ return false; }
+		if( preg_match('/'.preg_quote('Fatal error:', '/').'/si', $output) ){ return false; }
+		if( preg_match('/'.preg_quote('Warning:', '/').'/si', $output) ){ return false; }
+		if( preg_match('/'.preg_quote('Notice:', '/').'/si', $output) ){ return false; }
+		return true;
+	}
+
+
+	/**
+	 * コマンドを実行し、標準出力値を返す
+	 * @param array $ary_command コマンドのパラメータを要素として持つ配列
+	 * @return string コマンドの標準出力値
+	 */
+	private function passthru( $ary_command ){
+		set_time_limit(60*10);
+		$cmd = array();
+		foreach( $ary_command as $row ){
+			$param = escapeshellcmd($row);
+			array_push( $cmd, $param );
+		}
+		$cmd = implode( ' ', $cmd );
+		ob_start();
+		passthru( $cmd );
+		$bin = ob_get_clean();
+		set_time_limit(30);
+		return $bin;
+	}
+
+}
